a92de73b974ecb88e8eb51eef59dd5095f6c2efb
In69 <90973339+In69@users.noreply.github.com>
2021-12-07 10:06:48 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+
+
+if (age >= 18) {
+  console.log("Je mag naar binnen");
+} else {
+  console.log("Je mag niet naar binnen");
+}
+
+if (isFemale) {
+  console.log("Je bent een vrouw");
+} else {
+  console.log(
+    "Je mag niet naar binnen");
+}
+
+if (driverStatus === "bob") {
+  console.log("Je bent de bob");
+} else {
+  console.log("Je bent geen bob");
+}
